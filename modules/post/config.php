7ec5fa4a56d5e472969da0c0b28de334de2a8598
Iqbal Fauzi <iqbalfawz@gmail.com>
2020-02-16 15:39:11 +0700

    Add missing content pricing info

--- modules/post/config.php
+++ modules/post/config.php
@@ -2,7 +2,7 @@
 
 return [
     '__name' => 'post',
-    '__version' => '0.1.0',
+    '__version' => '0.1.1',
     '__git' => 'git@github.com:getmim/post.git',
     '__license' => 'MIT',
     '__author' => [
@@ -137,6 +137,19 @@ return [
                 ],
                 'created' => [
                     'type' => 'date'
+                ]
+            ],
+            'content-pricing' => [
+                'object' => [
+                    'cases' => [
+                        'post' => [
+                            'model' => [
+                                'name' => 'Post\\Model\\Post',
+                                'field' => 'id'
+                            ],
+                            'format' => 'post'
+                        ]
+                    ]
                 ]
             ]
         ]
